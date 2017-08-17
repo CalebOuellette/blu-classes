ee9c6d44eb72eb5bb6a9c67a6d2fa32508a823a5
CalebOuellette <Caleb.Ouellette@gmail.com>
2017-08-16 22:41:24 -0700

    changed date to number for Firebase

--- order.ts
+++ order.ts
@@ -3,7 +3,7 @@ import { Base, BaseProps } from './base';
 export class OrderProps extends BaseProps {
     public externalID: string; //ID to be used by our drivers and Management in order to have a clear understanding.
     public customerID: string; //
-    public orderTime: Date; //Time at which the customer confirmed the order.
+    public orderTime: number; //Time at which the customer confirmed the order.
     public paymentType: string;
     public from: string;
     public to: string;
