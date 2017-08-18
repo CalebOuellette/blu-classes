9906ee739f521783de254d9f3ac2ec13a92640e6
CalebOuellette <Caleb.Ouellette@gmail.com>
2017-08-17 19:28:16 -0700

    fixed type and comments

--- order_item.ts
+++ order_item.ts
@@ -1,9 +1,9 @@
 import { Base, BaseProps } from './base';
 
 export class OrderItemProps extends BaseProps {
-    public name: string; //ID to be used by our drivers and Management in order to have a clear understanding.
-    public price: string; //
-    public description: Date; //Time at which the customer confirmed the order.
+    public name: string; 
+    public price: string;
+    public description: string;    
     public productID?: string;
     public orderID: string;
 }
