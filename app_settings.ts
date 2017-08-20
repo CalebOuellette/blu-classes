c3ed245b63597f04f61ed772604f7aec12de4850
CalebOuellette <Caleb.Ouellette@gmail.com>
2017-08-20 00:08:49 -0700

    updates to classes and added app settings class

---
+++ app_settings.ts
@@ -0,0 +1,12 @@
+import { Base, BaseProps } from './base';
+
+export class AppSettingsProps extends BaseProps  {
+    public isOpen: boolean;
+    public deliveryFee: number;
+}
+
+
+export class AppSettings extends Base {
+    public static props: AppSettingsProps = new AppSettingsProps();
+    public static dbAddress: string = "AppSettings";
+}
--- index.ts
+++ index.ts
@@ -4,3 +4,4 @@ export * from "./order_item";
 export * from "./order";
 export * from "./product";
 export * from "./customer";
+export * from "./app_settings";
